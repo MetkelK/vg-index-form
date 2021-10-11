d6a338629036c3ad747b2ffa07dc7d3aba563b3c
Metkel K <metkelk@gmail.com>
2021-10-11 19:09:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Form from "./Components/Form";
 
-function App() {
+const App = () => {
+  const [systems, setSystems] = useState([]);
+  const [gameTitle, setgameTitle] = useState([]);
+
+  const queryParams = new URLSearchParams(window.location.search);
+  const discId = queryParams.get("discId");
+
+  useEffect(() => {
+    const getSystems = () => {
+      fetch(`https://vgindex-dev.org/api/change-request/systems`)
+        .then((res) => res.json())
+        .then((res) => {
+          setSystems(() => res);
+        })
+        .catch((err) => console.log(err));
+    };
+    getSystems();
+  }, []);
+
+  useEffect(() => {
+    const getGameTitle = () => {
+      fetch(`https://vgindex-dev.org/api/change-request/discs/${discId}`)
+        .then((res) => res.json())
+        .then((res) => {
+          setgameTitle(() => res);
+        })
+        .catch((err) => console.log(err));
+    };
+    getGameTitle();
+  }, [discId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Editing Disc {gameTitle.title}</h1>
+      <Form systems={systems} discId={discId} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,197 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Form(props) {
+  const { systems } = props;
+  let disc;
+  let featureFlags;
+
+  const [gameSystemId, setGameSystemId] = useState("");
+  const [discFormat, setDiscFormat] = useState("");
+  const [discFormatId, setDiscFormatId] = useState("");
+  const [discType, setDiscType] = useState("");
+  const [flags, setFlags] = useState([]);
+
+  useEffect(() => {
+    const getFormat = () => {
+      fetch(
+        `https://vgindex-dev.org/api/change-request/systems/${gameSystemId}/disc-formats`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setDiscFormat(() => res);
+        })
+        .catch((err) => console.log(err));
+    };
+    getFormat();
+  }, [gameSystemId]);
+
+  useEffect(() => {
+    const getFlags = () => {
+      fetch(
+        `https://vgindex-dev.org/api/change-request/systems/${gameSystemId}/feature-flags?discFormatId=${discFormatId}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setFlags(() => res);
+        })
+        .catch((err) => console.log(err));
+    };
+    getFlags();
+  }, [discFormatId, gameSystemId]);
+
+  //
+  if (discFormat && discFormat.status !== 404) {
+    disc = discFormat.map((disc, i) => (
+      <option key={i} value={disc.id}>
+        {disc.displayName}
+      </option>
+    ));
+  } else {
+    disc = <option>Choose a format</option>;
+  }
+
+  if (flags && flags.status !== 404 && flags && flags.status !== 400) {
+    featureFlags = flags.map((feature, i) => (
+      <div className="form-group pb-4" key={i}>
+        <label htmlFor={feature}>{feature}</label>
+        <textarea className="form-control" id={feature} rows="1"></textarea>
+      </div>
+    ));
+  } else {
+    featureFlags = null;
+  }
+
+  if (props) {
+    return (
+      <form className="">
+        <div className="form-group pb-2">
+          <label htmlFor="system">System</label>
+          <select
+            className="form-control"
+            id="system"
+            onChange={(e) => setGameSystemId(e.target.value)}
+            value={gameSystemId}
+          >
+            <option>Choose a system</option>
+            {systems.map((system, i) => (
+              <option key={i} value={system.id}>
+                {system.displayName}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-group pb-2">
+          <label htmlFor="format">Disc Format</label>
+          <select
+            className="form-control"
+            id="format"
+            value={discFormatId}
+            onChange={(e) => setDiscFormatId(e.target.value)}
+            disabled={!gameSystemId}
+          >
+            <option>Choose a format</option>
+            {disc}
+          </select>
+        </div>
+
+        <div className="form-group pb-2">
+          <label htmlFor="type">Disc Type</label>
+          <select
+            className="form-control"
+            id="type"
+            value={discType}
+            onChange={(e) => setDiscType(e.target.value)}
+            disabled={discFormat.status === 404 || discFormat.status === 400}
+          >
+            <option>Choose a type</option>
+            <option>Game</option>
+            <option>Demo</option>
+          </select>
+        </div>
+
+        <div className="form-group pb-4">
+          <p>Text Language(s)</p>
+
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value="English"
+              label="English"
+              id="textenglish"
+            ></input>
+            <label htmlFor="textenglish">English</label>
+          </div>
+
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value="Spanish"
+              label="Spanish"
+              id="textspanish"
+            ></input>
+            <label htmlFor="textspanish">Spanish</label>
+          </div>
+
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value="French"
+              label="French"
+              id="textfrench"
+            ></input>
+            <label htmlFor="textfrench">French</label>
+          </div>
+
+          <div className="form-group pb-4">
+            <p>Audio Language(s)</p>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="English"
+                label="English"
+                id="audioenglish"
+              ></input>
+              <label htmlFor="audioenglish">English</label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Spanish"
+                label="Spanish"
+                id="audiospanish"
+              ></input>
+              <label htmlFor="audiospanish">Spanish</label>
+            </div>
+
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="French"
+                label="French"
+                id="audiofrench"
+              ></input>
+              <label htmlFor="audiofrench">French</label>
+            </div>
+          </div>
+        </div>
+        {featureFlags}
+
+        <button type="submit" className="btn btn-outline-primary">
+          Submit for Review
+        </button>
+      </form>
+    );
+  } else return null;
+}
+
+export default Form;
