bbb01c3f810f683014f51457318fa5f56a5650c8
Metkel K <metkelk@gmail.com>
2021-10-19 01:15:46 -0400

    changed component name

--- src/Components/ChangeRequest.js
+++ src/Components/ChangeRequest.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownToggle from "@restart/ui/esm/DropdownToggle";
+import DropdownMenu from "@restart/ui/esm/DropdownMenu";
 
-function Form(props) {
+function ChangeRequest(props) {
   const { systems, title, languages } = props;
+
+  let gameName;
   let disc;
   let featureFlags;
   let gameType;
-  let gameName;
+  let languageOptions;
 
   const [gameSystemId, setGameSystemId] = useState("");
   const [discFormat, setDiscFormat] = useState("");
@@ -92,11 +99,74 @@ function Form(props) {
     gameName = <h1>Editing Disc</h1>;
   }
 
+  if (languages) {
+    languageOptions = languages.map((language, i) => (
+      <input
+        className="form-check-input me-1"
+        type="checkbox"
+        value={language.name}
+        label={language.name}
+        id={language.name}
+      ></input>
+    ));
+  } else {
+    languageOptions = null;
+  }
+
   if (props) {
     return (
       <>
         {gameName}
-        <form className="">
+        <Form>
+          <Form.Group className="form-group pb-2">
+            <Form.Label htmlFor="system">System</Form.Label>
+            <Form.Select
+              className="form-control"
+              id="system"
+              value={gameSystemId}
+              onChange={(e) => setGameSystemId(e.target.value)}
+            >
+              <option>Choose a system</option>
+              {systems.map((system, i) => (
+                <option key={i} value={system.id}>
+                  {system.displayName}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="form-group pb-2">
+            <Form.Label htmlFor="format">Disc Format</Form.Label>
+            <Form.Select
+              className="form-control"
+              id="format"
+              value={discFormatId}
+              onChange={(e) => setDiscFormatId(e.target.value)}
+              disabled={!gameSystemId}
+            >
+              <option>Choose a format</option>
+              {disc}
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="form-group pb-2">
+            <Form.Label htmlFor="type">Disc Type</Form.Label>
+            <Form.Select
+              className="form-control"
+              id="type"
+              value={discTypeId}
+              onChange={(e) => setDiscTypeId(e.target.value)}
+              disabled={!discFormatId}
+            >
+              <option>Choose a type</option>
+              {gameType}
+            </Form.Select>
+          </Form.Group>
+
+          {featureFlags}
+        </Form>
+
+        {/* <form className="">
           <div className="form-group pb-2">
             <label htmlFor="system">System</label>
             <select
@@ -143,39 +213,22 @@ function Form(props) {
           </div>
 
           <div className="form-group pb-4">
-            <p>Text Language(s)</p>
-
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value="English"
-                label="English"
-                id="textenglish"
-              ></input>
-              <label htmlFor="textenglish">English</label>
-            </div>
-
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value="Spanish"
-                label="Spanish"
-                id="textspanish"
-              ></input>
-              <label htmlFor="textspanish">Spanish</label>
-            </div>
-
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value="French"
-                label="French"
-                id="textfrench"
-              ></input>
-              <label htmlFor="textfrench">French</label>
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Text Language(s)
+              </button>
+              <ul
+                className="list-group dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                {languageOptions}
+              </ul>
             </div>
 
             <div className="form-group pb-4">
@@ -220,10 +273,10 @@ function Form(props) {
           <button type="submit" className="btn btn-outline-primary">
             Submit for Review
           </button>
-        </form>
+        </form> */}
       </>
     );
   } else return null;
 }
 
-export default Form;
+export default ChangeRequest;
