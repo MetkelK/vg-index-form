e0e228972bf67c2e077baf41b8fa1e52a5b4b246
Metkel K <metkelk@gmail.com>
2021-11-01 23:56:29 -0400

    cleaned up title component

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,12 +4,10 @@ import Form from "react-bootstrap/Form";
 function Title(props) {
   const { title } = props;
   return (
-    <div>
-      <Form.Group className="mb-3" controlId="title">
-        <Form.Label>Title</Form.Label>
-        <Form.Control type="input" placeholder={title} />
-      </Form.Group>
-    </div>
+    <Form.Group className="mb-3" controlId="title">
+      <Form.Label>Title</Form.Label>
+      <Form.Control type="input" placeholder={title} />
+    </Form.Group>
   );
 }
 
