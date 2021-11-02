b22ae31a940080793710ef49f4608276dcddf2e8
Metkel K <metkelk@gmail.com>
2021-11-02 01:04:16 -0400

    created system component

---
+++ src/Components/System.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+function System(props) {
+  const { systems, currentSystem } = props;
+
+  const [system, setSystem] = useState("");
+
+  let systemName;
+
+  if (currentSystem && systems.length > 0) {
+    systemName = systems.find((system) => {
+      return system.id === currentSystem;
+    });
+  } else {
+    systemName = "Choose a system";
+  }
+
+  return (
+    <Form.Group className="my-2">
+      <Form.Label htmlFor="system">System</Form.Label>
+      <Form.Select
+        id="system"
+        value={currentSystem ? currentSystem : system}
+        onChange={(e) => setSystem(e.target.value)}
+      >
+        <option value={currentSystem}>{systemName.name}</option>
+        {systems.map((system, i) => (
+          <option key={i} value={system.id}>
+            {system.name}
+          </option>
+        ))}
+      </Form.Select>
+    </Form.Group>
+  );
+}
+
+export default System;
